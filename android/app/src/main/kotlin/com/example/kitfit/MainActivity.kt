8cb14e0b017c560c0b7e9ca6d2acb35cdc2b7066
Pape Moussa Gadiaga <papemoussagadiaga@MacBook-Air-de-Pape.local>
2024-01-24 10:02:03 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/kitfit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kitfit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/PerformanceApp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class PerformanceApp extends StatelessWidget {
+  const PerformanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
